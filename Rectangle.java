a49b8624b9292fb3287b170f48ed6717e0c1259d
Odin Hultgren Van Der Horst <odinhorst@hotmail.com>
2016-10-10 22:34:28 +0200

    Added Rectangle Added and immplomented Rectangle.java
            -Added and immplomented Rectangle.java(A abstract class
    containg the info and functions for a rectangle).

---
+++ Rectangle.java
@@ -0,0 +1,25 @@
+/* Rectangle abstract class
+ * Ver 1.0
+ */
+
+abstract class Rectangle extends GeometricObject{
+    private double width;
+    private double height;
+    
+    //sets the width to a possitive version of w
+    void setWidth(double w){width = w > 0 ? w : w * -1;}
+    
+    //sets the height to a possitive version of h
+    void setHeight(double h){height = h > 0 ? h : h * -1;}
+    
+    double getWidth(){return width;}
+
+    double getHeight(){return height;}
+
+    //returns the area of the Rectangle
+    double findArea(){return width*height;}
+    
+    //returns the circumfirance of the Rectangle by multiplying the biggest of 
+    //height and width with 2*PI
+    double findCircumfirance(){return 2*Math.PI* (width > height ? width : height);}
+}
