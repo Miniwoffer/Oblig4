0bc565e0e64a6ca757a6db2b433f03e8141cfbe9
Odin Hultgren Van Der Horst <odinhorst@hotmail.com>
2016-10-11 20:45:01 +0200

    Added and implomented Ball.java
            - Added Ball.java(and immplomented it according to the UML)

---
+++ Ball.java
@@ -0,0 +1,38 @@
+
+class Ball extends Circle 
+{
+    double thickness;
+    Ball()
+    {
+        Ball(5,2);
+    }
+    Ball(double r,double t)
+    {
+        super(5);
+        thickness = 2;
+    }
+    double findVolum()
+    {
+        double innerVolum = 0;
+        if(thickness > 0)
+        {
+            innerVolum = 4/3*Math.PI*Math.Pow(radius-thickness,3);
+        }
+        return 4/3*Math.PI*Math.Pow(radius,3)-innerVolum;
+    }
+    double findSurfaceArea()
+    {
+        double innerSurface = 0;
+        if(thickness > 0)
+        {
+            innerSurface = 4*Math.PI*Math.Pow(radius-thickness,2);
+        }
+        return 4*Math.PI*Math.Pow(radius,2)+innerSurface;
+    }
+    double findWeight()
+    {
+        return findVolum()*density;
+    }
+    void setThickness(double t){thickness = t > 0 && t < radius ? t : 0;}
+    double getThickness(){return thickness;}
+}
