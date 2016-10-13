4354f20a50d19120ed427b7ef51728445bb2128f
Petter Berven <petterdberven@hotmail.com>
2016-10-13 11:47:48 +0200

    gjennopprettet det jeg slettet.

---
+++ UnitTest.java
@@ -0,0 +1,14 @@
+class UnitTest
+{
+    
+    public static void main(String[] args)
+    {
+        testBall();
+    }
+    
+    static void testBall()
+    {
+        Ball b = new Ball(5,2);
+        System.out.println(b.toString());
+    }
+}
