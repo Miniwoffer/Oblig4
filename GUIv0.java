d468172b20fbb55f44db91b75a8bc656c5c79291
Petter Duus Berven <Petter Duus Berven>
2016-10-14 13:56:03 +0200

    GUIv0 testing file Added testing file with basic usability

--- GUIv0.java
+++ GUIv0.java
@@ -1,16 +1,50 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.shape.Sphere;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.AmbientLight;
+import javafx.scene.PointLight;
+import javafx.scene.paint.Color;
+import javafx.scene.input.MouseEvent;
+import java.util.EventListener;
+import javafx.event.EventHandler;
+
 
 
 public class GUIv0 extends Application {
 
+	public Group root;
+
 	@Override
 	public void start(Stage stage) {
-		Scene scene = new Scene(200, 200);
+		Sphere sphere = new Sphere(100); 
+		Sphere sphere2 = new Sphere(50);
+
+		root = new Group(sphere, sphere2);
+
+		root.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent t) {
+				System.out.println("l");
+			}
+		});
+
+		Scene scene = new Scene(root, 600, 600);
+		root.setTranslateX(300);
+		root.setTranslateY(300);
+
+		PhongMaterial phong = new PhongMaterial(Color.color(1,0,0,0.5));
+		//sphere.setMaterial(phong);
+		PhongMaterial phong2 = new PhongMaterial(Color.color(0,1,0,0.8));
+		sphere2.setMaterial(phong2);
+
+
 
 		stage.setTitle("GUIv0");
 		stage.setScene(scene);
+		stage.show();
 	}
 
 	public static void main(String[] args) {
@@ -18,3 +52,8 @@ public class GUIv0 extends Application {
 	}
 }
 
+class Kkk implements EventListener {
+	public void onDragDetected(MouseEvent m) {
+		System.out.println(m.toString());
+	}
+}
