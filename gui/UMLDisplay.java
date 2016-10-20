7a6f2c576d9bb2721a0a0ced923a706c61cbbb94
Petter Berven <buster1253@github.com>
2016-10-20 08:38:56 +0200

    UMLDisplay done

--- gui/UMLDisplay.java
+++ gui/UMLDisplay.java
@@ -3,6 +3,8 @@ package gui;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.Group;
+import javafx.geometry.Insets;
+
 
 
 
@@ -11,6 +13,10 @@ public class UMLDisplay extends Display {
 		Image img = new Image("images/UML.png");
 		ImageView view = new ImageView();
 		view.setImage(img);
+		view.setY(30);
+		view.setFitHeight(570);
+		view.setFitWidth(600);
+		//view.setPadding(new Insets(40, 0,0,0));
 
 		root = new Group(view);
 	}
